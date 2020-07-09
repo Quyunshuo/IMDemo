de0d5091862bbe96a2440f18b1384d0f982304b5
Quyunshuo <quyunshuo@qq.com>
2020-07-09 19:04:31 +0800

    Modify: 初始化-登录

--- app/src/main/java/com/quyunshuo/imdemo/BaseApplication.kt
+++ app/src/main/java/com/quyunshuo/imdemo/BaseApplication.kt
@@ -1,12 +1,38 @@
 package com.quyunshuo.imdemo
 
 import android.app.Application
+import android.util.Log
+import com.quyunshuo.imdemo.GenerateTestUserSig.SDKAPPID
+import com.tencent.imsdk.v2.V2TIMManager
+import com.tencent.imsdk.v2.V2TIMSDKConfig
+import com.tencent.imsdk.v2.V2TIMSDKListener
 
 class BaseApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        // 初始化config对象
+        val config = V2TIMSDKConfig()
+        // 指定log输出级别
+        config.setLogLevel(V2TIMSDKConfig.V2TIM_LOG_DEBUG)
+        // 初始化 sdk 并设置 V2TIMSDKListener 的监听对象
+        // initSDK 后 SDK 会自动连接网络，网络连接状态可以在 V2TIMSDKListener 回调里面监听
+        V2TIMManager.getInstance().initSDK(this, SDKAPPID, config, object : V2TIMSDKListener() {
 
+            // 正在连接到腾讯云服务器
+            override fun onConnecting() {
+                Log.d("qqq", "onConnecting: ")
+            }
 
+            // 已经成功连接到腾讯云服务器
+            override fun onConnectSuccess() {
+                Log.d("qqq", "onConnectSuccess: ")
+            }
+
+            // 连接腾讯云服务器失败
+            override fun onConnectFailed(code: Int, error: String?) {
+                Log.d("qqq", "onConnectFailed: ")
+            }
+        })
     }
 }
--- app/src/main/java/com/quyunshuo/imdemo/MainActivity.kt
+++ app/src/main/java/com/quyunshuo/imdemo/MainActivity.kt
@@ -2,10 +2,35 @@ package com.quyunshuo.imdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.quyunshuo.imdemo.databinding.ActivityMainBinding
+import com.tencent.imsdk.v2.V2TIMCallback
+import com.tencent.imsdk.v2.V2TIMManager
 
 class MainActivity : AppCompatActivity() {
+
+    private val mBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(mBinding.root)
+        initView()
+    }
+
+    private fun initView() {
+        mBinding.loginBtn.setOnClickListener {
+            V2TIMManager.getInstance().login(
+                "1001",
+                GenerateTestUserSig.genTestUserSig("1001"),
+                object : V2TIMCallback {
+                    override fun onSuccess() {
+                        Log.d("qqq", "onSuccess: ")
+                    }
+
+                    override fun onError(p0: Int, p1: String?) {
+                        Log.d("qqq", "onError: ")
+                    }
+                })
+        }
     }
 }
