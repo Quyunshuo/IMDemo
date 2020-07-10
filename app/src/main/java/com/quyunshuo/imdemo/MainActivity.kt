c75662286c5b9dd1b9ed236a9cd7f4f184eccb14
Quyunshuo <quyunshuo@qq.com>
2020-07-10 09:47:52 +0800

    Modify: 创建临时会议群组及解散

--- app/src/main/java/com/quyunshuo/imdemo/MainActivity.kt
+++ app/src/main/java/com/quyunshuo/imdemo/MainActivity.kt
@@ -1,15 +1,17 @@
 package com.quyunshuo.imdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.quyunshuo.imdemo.databinding.ActivityMainBinding
-import com.tencent.imsdk.v2.V2TIMCallback
-import com.tencent.imsdk.v2.V2TIMManager
+import com.tencent.imsdk.v2.*
+
 
 class MainActivity : AppCompatActivity() {
 
     private val mBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    private var mGroupId: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,5 +35,54 @@ class MainActivity : AppCompatActivity() {
                     }
                 })
         }
+        mBinding.createGroupBtn.setOnClickListener { createGroup() }
+        mBinding.dissolveGroupBtn.setOnClickListener {
+            if (mGroupId.isNotEmpty()) {
+                dissolveGroup(mGroupId)
+            }
+        }
+    }
+
+    /**
+     * 创建群组
+     */
+    private fun createGroup() {
+        val groupInfo = V2TIMGroupInfo()
+        // 组名
+        groupInfo.groupName = "testGroup"
+        // 组类型 临时会议群（Meeting）
+        groupInfo.groupType = "Meeting"
+        // 组简介
+        groupInfo.introduction = "这是一个测试群组"
+        groupInfo.groupID = "100001"
+        V2TIMManager.getGroupManager().createGroup(
+            groupInfo, ArrayList(), object : V2TIMValueCallback<String?> {
+                override fun onError(code: Int, desc: String) {
+                    // 创建失败
+                    Log.d("qqq", "onError: code:$code msg: $desc")
+                }
+
+                override fun onSuccess(groupID: String?) {
+                    // 创建成功
+                    Log.d("qqq", "onSuccess: groupID:$groupID")
+                    groupID?.let { mGroupId = it }
+                }
+            })
+    }
+
+    /**
+     * 解散群组
+     * @param groupId 群组id
+     */
+    private fun dissolveGroup(groupId: String) {
+        V2TIMManager.getInstance().dismissGroup(groupId, object : V2TIMCallback {
+            override fun onSuccess() {
+                Log.d("qqq", "onSuccess: 解散成功")
+            }
+
+            override fun onError(p0: Int, p1: String?) {
+                Log.d("qqq", "onError: 解散失败 code: $p0 msg: $p1")
+            }
+        })
     }
 }
