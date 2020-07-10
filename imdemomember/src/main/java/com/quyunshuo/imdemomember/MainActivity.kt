69552b1bfe936bc14cb0b4d82ba6d7c0d457cc6c
Quyunshuo <quyunshuo@qq.com>
2020-07-10 09:58:49 +0800

    Modify: 加群操作

--- imdemomember/src/main/java/com/quyunshuo/imdemomember/MainActivity.kt
+++ imdemomember/src/main/java/com/quyunshuo/imdemomember/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initView() {
-        // 群主
+        // 成员
         mBinding.member1LoginBtn.setOnClickListener {
             V2TIMManager.getInstance().login(
                 "2001",
@@ -33,5 +33,22 @@ class MainActivity : AppCompatActivity() {
                     }
                 })
         }
+        mBinding.joinGroupBtn.setOnClickListener { joinGroup("100001", "测试加群") }
+    }
+
+    /**
+     * 加群
+     * @param groupId 群组id
+     */
+    private fun joinGroup(groupId: String, msg: String) {
+        V2TIMManager.getInstance().joinGroup(groupId, msg, object : V2TIMCallback {
+            override fun onSuccess() {
+                Log.d("qqq", "onSuccess: 加群成功")
+            }
+
+            override fun onError(p0: Int, p1: String?) {
+                Log.d("qqq", "onError: 加群失败 code: $p0 msg: $p1")
+            }
+        })
     }
 }
